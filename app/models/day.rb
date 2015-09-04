0ea08ec28073963a11450e896d9d10e17bdc5d00
Alvaro <pursuance@gmail.com>
2015-09-05 01:05:40 +0200

    fixing gmt2

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,16 +1,18 @@
 class Day < ActiveRecord::Base
   validate :just_today, on: :create
   GMT = 2
+  GMT_SECONDS = GMT * 60 * 60
+
 
   def just_today
   end
 
   def self.today
-    Day.select{ |day| day.created_at.to_date == (Time.now + GMT * 60 * 60).to_date }.first
+    Day.select{ |day| day.created_at.to_date == (Time.now + GMT_SECONDS).to_date }.first
   end
 
   def human_date
-    created_at.strftime("%A, %e %B %Y")
+    (created_at + GMT_SECONDS).strftime("%A, %e %B %Y")
   end
 
   def is_html?
