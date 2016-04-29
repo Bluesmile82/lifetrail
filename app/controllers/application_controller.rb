c60329070ea2f2502c14b201937c02e6d4571185
Bluesmile <pursuance@gmail.com>
2016-04-29 20:12:13 +0200

    Disables authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
   before_action :set_locale
-  http_basic_authenticate_with name: ENV['user'], password: ENV['password']
+  # http_basic_authenticate_with name: ENV['user'], password: ENV['password']
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
